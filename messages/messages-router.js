88e6e5dcec3796ac315bf5a15669cd78db32ef63
Dodger-42 <kevin.bing.barber@gmail.com>
2020-12-15 18:37:16 -0800

    class

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -3,24 +3,70 @@ const db = require("../data/config")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
-
+router.get("/", async (req, res, next) => {
+    try{
+     const messages = await db
+     .select("*")
+     .from("messages")
+     res.json(messages)
+    }catch (err) {
+        next(err)
+    }
 })
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
+    try{
+        const [message] = await db
+        .select("*")
+        .from("messages")
+        .where("id", req.params.id)
+        .limit(1)
 
+        res.json(message)
+    }catch (err) {
+        next(err)
+    }
 })
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+    try{
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
 
+        const [id] =await  db.insert(payload).into("messages")
+
+        const message = await db("messages").first().where("id", id)
+        res.status(201).json(message)
+
+    }catch (err) {
+        next(err)
+    }
 })
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
+    try{
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
+        await db("messages").first().where("id", req.params.id).update(payload)
 
+    }catch (err) {
+        next(err)
+    }
 })
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
+    try{
 
+        const message = await db("messages").first().where("id", req.params.id)
+        res.json(message)
+
+    }catch (err) {
+        next(err)
+    }
 })
 
 module.exports = router
